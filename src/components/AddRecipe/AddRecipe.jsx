8c175db52d2ef1fe1c211aa3c26faf5e948c076c
TomaszSlupik <99093128+TomaszSlupik@users.noreply.github.com>
2022-11-10 09:11:36 +0100

    change

--- src/components/AddRecipe/AddRecipe.jsx
+++ src/components/AddRecipe/AddRecipe.jsx
@@ -7,6 +7,8 @@ import zIndex from '@mui/material/styles/zIndex'
 
 const MAX_NAME_LENGTH = 45
 const MIN_NAME_LENGTH = 4
+const MIN_DESCRIPTION_LENGTH = 20
+const MAX_DESCRIPTION_LENGTH = 150
 
 const styles = {
     div: {display: 'flex', flexDirection: 'column', alignItems: 'center'},
@@ -22,7 +24,7 @@ export const AddRecipe = (props) => {
         if (value !== validvalue) {
             setName(validvalue)
         }
-        const isError = !value || value.length < MIN_NAME_LENGTH
+        const isError = !validvalue || validvalue.length < MIN_NAME_LENGTH
         setNameError(isError)
         return isError
     }
@@ -32,6 +34,39 @@ export const AddRecipe = (props) => {
         }
     }
 
+    const [description, setDescription] = React.useState('')
+    const [descriptionError, setDescriptionError] = React.useState(false)
+    const descriptionValue = (value) => {
+        const validvalue = value && value.replace (/\s{2,}/g, '')
+        if (value !== validvalue) {
+            setDescription(validvalue)
+        }
+        const isError = !validvalue || validvalue.length < MIN_DESCRIPTION_LENGTH
+        setDescriptionError(isError)
+        return isError
+    }
+    const setValidDescription = string => {
+        if (string.length < MAX_DESCRIPTION_LENGTH) {
+            setDescription(string)
+        }
+    }
+
+    const [time, setTime] = React.useState('')
+    const [timeError, setTimeError] = React.useState(false)
+
+    const timeValue = () => {
+
+    }
+
+    const setValidTime = string => {
+        setTime(string)
+    }
+
+
+
+
+
+
     const inputs = [
         {
             label: 'Nazwa',
@@ -39,7 +74,27 @@ export const AddRecipe = (props) => {
             onChange: setValidName, 
             error: nameError,
             validate: nameValidate,
-            helperText: 'Zbyt krótka nazwa, min. 4 znaki'
+            helperText: 'Zbyt krótka nazwa, min. 4 znaki', 
+            multiline: false
+        }, 
+        {
+            label: 'Sposób przyrządzenia', 
+            value: description, 
+            onChange: setValidDescription, 
+            error: descriptionError, 
+            validate: descriptionValue, 
+            helperText: 'Zbyt krótki opis', 
+            multiline: true
+        },
+        {
+            label: 'Czas przygotowania', 
+            value: time, 
+            onChange: setValidTime, 
+            error: setTimeError, 
+            validate: timeValue, 
+            helperText: 'Podaj czas', 
+            multiline: false, 
+            type: 'number'
         }
     ]
 
@@ -63,6 +118,8 @@ export const AddRecipe = (props) => {
         }
         }}
         onBlur={()=> el.validate(el.value)}
+        multiline={el.multiline}
+        type={el.type || 'text'}
         />
         ))
         }
